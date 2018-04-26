3bd9f3193fe6c4f7c8c285b77a901594383a4ea9
mindip <oacidnim@g.ucla.edu>
2018-04-26 01:43:41 -0700

    skeleton code for login enter animation

--- src/containers/app.js
+++ src/containers/app.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <LoginPage></LoginPage>
-        <EditorPage></EditorPage>
+        {/* <EditorPage></EditorPage> */}
       </div>
     );
   }
--- src/containers/login.js
+++ src/containers/login.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Login from '../components/Login'
+import CSSTransition from 'react-transition-group/CSSTransition'
+import '../styles/login.css';
 
 class LoginPage extends React.Component {
+
+  state = {
+    loggedIn : false
+  }
+
   render() {
-    return(
-      <div>
+    const { loggedIn } = this.state
+    return (
+    // Read the doc on your ticket to figure out what's happening here.
+    <CSSTransition in={!loggedIn} timeout={300} classNames="login" unmountOnExit>
+    <div className="login-container">
         <Login></Login>
-      </div>
+        {/* DELETE this button in your final commit as the oauth ticket will make an actual login button.
+          This button "exits" the login component. We'll work with them to finalize the animation. */}
+        <button onClick={() => {this.setState(state =>({ loggedIn: true}))}}> TEST EXIT </button>
+    </div>
+    </CSSTransition>
     )
   }
 }
