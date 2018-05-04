a77447723c1177d08dd5df0cc0fbb301739c7e81
jamieliu386 <jamie.liu386@gmail.com>
2018-05-03 17:48:44 -0700

    finish editor animation

--- src/containers/editor.js
+++ src/containers/editor.js
@@ -12,19 +12,12 @@ class EditorPage extends React.Component {
   render() {
     const { loggedIn } = this.state
     return(
-      // Delete this outer container when you finish
-      <div>
-      {/* DELETE this button in your final commit as the oauth ticket will make an actual login button. */}
-      {/* This button "exits" the login component. We'll work with them to finalize the animation. */}
-      <button onClick={() => {this.setState(state =>({ loggedIn: true}))}}> TEST ENTER </button>
-       
-      {/* Read the doc on your ticket to figure out what's happening here. */}
+
       <CSSTransition in={loggedIn} timeout={300} classNames="editor" unmountOnExit>
         <div className="editor-container">
           <Editor></Editor>
         </div>
       </CSSTransition> 
-      </div>
     )
   }
 }
