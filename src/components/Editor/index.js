9450ac95635cc0c0b1f1296bffcc3a0a0a5ccd2b
Connor Borden <connorbo97@gmail.com>
2018-05-20 11:16:38 -0700

    Dock slider

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -13,12 +13,29 @@ class Editor extends React.Component {
     this.state = {
       code: "// Code",
       isVisible:true,
+      size:0.25,
+      prevSize:0.25,
     };
     this.options = {
       mode: 'javascript',
       theme: 'material',
       lineNumbers: true
     };
+  }
+
+  handleOnVisibleChange = () => {
+    this.setState({
+      isVisible: !this.state.isVisible,
+      size:this.state.isVisible ? 0.0 : 0.25,
+      prevSize:this.state.isVisible ? 0.0 : 0.25,
+    })
+  }
+  
+  handleOnSizeChange = (newSize) => {
+    this.setState({
+      size:newSize,
+      prevSize:this.state.size,
+    })
   }
 
 	updateCode = (newCode) => {
@@ -32,40 +49,63 @@ class Editor extends React.Component {
   }
 
 	render() {
-    let {isVisible} = this.state
+    console.log(this.state.size)
+    let {isVisible, size, prevSize} = this.state
     let {logout, user} = this.props
 
+    //panelSize is how much of the screen the panel will take up
+    let panelSize=(size*100.0).toString() + '%'
+
+    let panelStyle = {width:panelSize}            //width of the panel should always be the percent of its size (which is a decimal)
+    let codeStyle = {
+      position:"fixed",                           //fixed bc we're using left
+      width:"100%",                               //take up 100% of whats given
+      left:panelSize,                             //panelSize determines how far left of the screen the code should be
+      transition:(prevSize != size && (size != 0.0 || prevSize != 0.0)) ? "" : "left 0.2s ease-out, opacity 0.01s linear" //if they're using the slider to change the length of the panel, dont use a transition, otherwise (meaning they're using the toggle button) use a transition where when the left changes, it eases out
+    }
     return(
       <div className="editor">
-        <Dock position='left' isVisible={isVisible} dimMode="opaque" onVisibleChange={() => this.setState({ isVisible: !isVisible })}>
+        <div style={panelStyle}>
+          <Dock position='left'
+                isVisible={isVisible}
+                size={size}
+                dimMode="transparent"
+                onSizeChange={(newSize)=>{this.handleOnSizeChange(newSize)}}
+                onVisibleChange={this.handleOnVisibleChange}
+                dockStyle={{width:panelSize}}
+          >
             <div className="panel">
               <div className="panel-collapse-button">
-                <div/><div onClick={() => this.setState({ isVisible: !isVisible })}>&larr;</div>
+                <div/><div onClick={this.handleOnVisibleChange}>&larr;</div>
               </div>
-              <img className="panel-image" src={user.photoURL || "img/defaultProfile.png"}/>
-              <div className="panel-name">{user.displayName || "Joe Bruin"}</div>
-              <div className="panel-options">
-                <ul className="panel-options-list">
-                  <li className="panel-options-item">Profile</li>
-                  <li className="panel-options-item">Sketches</li>
-                  <li className="panel-options-item">Signout</li>
-                </ul>
+              <div className="panel-content">
+                <img className="panel-image" src={user.photoURL || "img/defaultProfile.png"}/>
+                <div className="panel-name">{user.displayName || "Joe Bruin"}</div>
+                <div className="panel-options">
+                  <ul className="panel-options-list">
+                    <li className="panel-options-item">Profile</li>
+                    <li className="panel-options-item">Sketches</li>
+                    <li className="panel-options-item" onClick={logout}>Signout</li>
+                  </ul>
+                </div>
               </div>
             </div>
-        </Dock>
-        <button onClick={logout}>Logout</button>
-        <button onClick={()=>{this.setState({isVisible:true})}}>Open Dock</button>
-        <CodeMirror
-          value={this.state.code}
-          options={this.options}
-          onBeforeChange={(editor, data, code) => {
-            this.setState({code});
-          }}
-          onChange={(editor, data, code) => {
-            this.updateCode(code);
-          }}
-        />
-        <input type="button" value="Run" onClick={this.runCode}/>
+          </Dock>
+        </div>
+        <div style={codeStyle}>
+          {isVisible ? <div className='editor-expand-panel'>&nbsp;</div> : <div className='editor-expand-panel' onClick={this.handleOnVisibleChange}>></div>}
+          <CodeMirror
+            value={this.state.code}
+            options={this.options}
+            onBeforeChange={(editor, data, code) => {
+              this.setState({code});
+            }}
+            onChange={(editor, data, code) => {
+              this.updateCode(code);
+            }}
+          />
+          <input type="button" value="Run" onClick={this.runCode}/>
+        </div>
       </div>
     );
 	}
