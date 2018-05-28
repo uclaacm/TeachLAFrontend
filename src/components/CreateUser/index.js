633feca9828a65598d257239c6094034994ecd0b
Connor Borden <connorbo97@gmail.com>
2018-05-28 14:06:40 -0700

    Input checking for creating user

--- src/components/CreateUser/index.js
+++ src/components/CreateUser/index.js
@@ -5,9 +5,19 @@ import SocialButton from '../SocialButton'
 import firebase from 'firebase'
 import AES from 'crypto-js/aes'
 import SHA256 from 'crypto-js/sha256'
+import Filter from 'bad-words'
 // import '../styles/app.css'
 import '../../styles/CreateUser.css'
-import {SERVER_URL, MINIMUM_USERNAME_LENGTH} from '../../constants';
+import {SERVER_URL,
+    MINIMUM_USERNAME_LENGTH,
+    MINIMUM_PASSWORD_LENGTH,
+    MINIMUM_DISPLAY_NAME_LENGTH,
+    MAXIMUM_USERNAME_LENGTH,
+    MAXIMUM_PASSWORD_LENGTH,
+    MAXIMUM_DISPLAY_NAME_LENGTH,
+} from '../../constants';
+
+const filter = new Filter();
 
 class CreateUser extends React.Component {
 	constructor(props){
@@ -25,6 +35,57 @@ class CreateUser extends React.Component {
 
         this.handleFetchSuccess = this.handleFetchSuccess.bind(this)
         this.handleFetchFailure = this.handleFetchFailure.bind(this)
+    }
+
+    checkInputs = () => {
+        const {username, password, displayName} = this.state
+        let badInputs = false
+
+        if(username.length < MINIMUM_USERNAME_LENGTH){
+            this.setState({usernameMessage:`Username must be at least ${MINIMUM_USERNAME_LENGTH} characters`})
+            badInputs = true
+        } else if(username.length > MAXIMUM_USERNAME_LENGTH){
+            this.setState({usernameMessage:`Username must be at most ${MAXIMUM_USERNAME_LENGTH} characters`})
+            badInputs = true
+        } else if(username.match(/[^a-zA-Z0-9!@#$%]/)){
+            this.setState({usernameMessage:"Username must only use upper case and lower case letters, numbers, and/or the special characters !@#$%"})
+            badInputs = true
+        } else if(filter.isProfane(username)) {
+            this.setState({usernameMessage:"Username must not contain profanity"})
+            badInputs = true
+        } else {
+            this.setState({usernameMessage:null})
+        }
+
+        if(password.length < MINIMUM_PASSWORD_LENGTH){
+            this.setState({passwordMessage:`Password must be at least ${MINIMUM_PASSWORD_LENGTH} characters`})
+            badInputs = true
+        } else if(password.length > MAXIMUM_PASSWORD_LENGTH){
+            this.setState({passwordMessage:`Password must be at most ${MAXIMUM_PASSWORD_LENGTH} characters`})
+            badInputs = true
+        } else if(password.match(/[^a-zA-Z0-9!@#$%]/)){
+            this.setState({passwordMessage:"Password must only use upper case and lower case letters, numbers, and/or the special characters !@#$%"})
+            badInputs = true
+        } else {
+            this.setState({passwordMessage:null})
+        }
+
+        if(displayName.length < MINIMUM_DISPLAY_NAME_LENGTH){
+            this.setState({displaynameMessage:`Display Name must be at least ${MINIMUM_DISPLAY_NAME_LENGTH} characters`})
+            badInputs = true
+        } else if(displayName.length > MAXIMUM_DISPLAY_NAME_LENGTH){
+            this.setState({displaynameMessage:`Display Name must be at most ${MAXIMUM_DISPLAY_NAME_LENGTH} characters`})
+            badInputs = true
+        } else if(displayName.match(/[^a-zA-Z0-9!@#$% ]/)){
+            this.setState({displaynameMessage:"Display Name must only use upper case and lower case letters, numbers, spaces, and/or the special characters !@#$%"})
+            badInputs = true
+        } else if(filter.isProfane(displayName)) {
+            this.setState({displaynameMessage:"Display Name must not contain profanity"})
+            badInputs = true
+        } else {
+            this.setState({displaynameMessage:null})
+        }
+        return badInputs
     }
     
     handleFetchSuccess = async function(response){
@@ -64,45 +125,50 @@ class CreateUser extends React.Component {
         console.log(username.match(/[^a-zA-Z0-9!@#$%]/))
         console.log(username.length)
         // console.log(username.match(/[^a-zA-Z\d]/))
+
+        let badInputs = this.checkInputs()
+
+        //if we found any bad inputs, don't try to create the user on the server
+        if(badInputs){
+            return
+        }
+
+        this.setState({waiting:true, message:null})
+
+        let url = `${SERVER_URL}/createUser`
+        
+        /**
+         * createRequest json as follows
+         * {
+         *  disabled: boolean
+         *  displayName: string
+         *  email: string
+         *  emailVerified: boolean
+         *  password: string
+         *  phoneNumber: string
+         *  photoURL: string
+         *  uid: string
+         * }
+         */
+        
+        let content = {
+            uid: SHA256(username).toString(),
+            password: SHA256(password).toString(),
+            displayName: displayName,
+        }
         
 
-
-        // this.setState({waiting:true, message:null})
-
-        // let url = `${SERVER_URL}/createUser`
-        
-        // /**
-        //  * createRequest json as follows
-        //  * {
-        //  *  disabled: boolean
-        //  *  displayName: string
-        //  *  email: string
-        //  *  emailVerified: boolean
-        //  *  password: string
-        //  *  phoneNumber: string
-        //  *  photoURL: string
-        //  *  uid: string
-        //  * }
-        //  */
-        
-        // let content = {
-        //     uid: SHA256(username).toString(),
-        //     password: SHA256(password).toString(),
-        //     displayName: displayName,
-        // }
-        
-
-        // let init = {
-        //     method: 'POST',
-        //     body: JSON.stringify(content),
-        //     headers: {
-        //       'content-type': 'application/json'
-        //     },
-        // }
+        let init = {
+            method: 'POST',
+            body: JSON.stringify(content),
+            headers: {
+              'content-type': 'application/json'
+            },
+        }
     
-        // return fetch(url, init)
-        //     .then(this.handleFetchSuccess)
-        //     .catch(this.handleFetchFailure)
+        return fetch(url, init)
+            .then(this.handleFetchSuccess)
+            .catch(this.handleFetchFailure)
     }
 
 	render() {
@@ -123,25 +189,24 @@ class CreateUser extends React.Component {
                                     Username
                                 </div>
                                 <input className="create-form-input" type="text" name="username"
-                                    placeholder="firstnamelastnameschool" value={this.state.username}
+                                    placeholder="" value={this.state.username}
                                     onChange={(e)=>{this.setState({username:e.target.value})}}
                                 />
-                                {usernameMessage ? <div style={{color:'red'}}>{usernameMessage}</div> : <br/>}
+                                {usernameMessage ? <div style={{color:'red', fontSize:'0.8em'}}>{usernameMessage}</div> : <br/>}
                                 <div className="create-form-input-header">Password</div>
                                 <input className='create-form-input' type="password" name="password"
                                     placeholder="" value={this.state.password}
                                     onChange={(e)=>{this.setState({password:e.target.value})}}
                                 />
-                                {passwordMessage ? <div style={{color:'red'}}>{passwordMessage}</div> : <br/>}
+                                {passwordMessage ? <div style={{color:'red', fontSize:'0.8em'}}>{passwordMessage}</div> : <br/>}
                                 <div className="create-form-input-header">
                                     Display Name
                                 </div>
-                                {displaynameMessage ? <div style={{color:'red'}}>{displaynameMessage}</div> : <br/>}
                                 <input className='create-form-input' type="text" name="displayname"
                                     placeholder="First Last" value={this.state.displayName}
                                     onChange={(e)=>{this.setState({displayName:e.target.value})}}
                                 />
-                                <br/>
+                                {displaynameMessage ? <div style={{color:'red', fontSize:'0.8em'}}>{displaynameMessage}</div> : <br/>}
                             </div>
                             <RingLoader
                             color={'#171124'} 
