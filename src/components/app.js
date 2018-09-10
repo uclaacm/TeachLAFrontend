792f8a213b992c3b2cc4232913e3c5e9c956a159
Connor Borden <connorbo97@gmail.com>
2018-09-10 10:03:29 -0700

    updated app js to new loading page file path

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import LoginPage from './containers/LoginContainer';
 import EditorPage from './containers/EditorContainer';
-import LoadingPage from './containers/Loading.js';
+import LoadingPage from './common/LoadingPage';
 import CreateUserPage from './containers/CreateUserContainer';
 import firebase from 'firebase'
 import '../styles/app.css'
