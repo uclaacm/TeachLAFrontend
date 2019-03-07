47cd9f423b1a5cd61e02866f363287eb0fb47919
Alyssa <alywang27@gmail.com>
2019-03-07 00:28:01 -0800

    not sure why refresh not working

--- src/reducers/userDataReducer.js
+++ src/reducers/userDataReducer.js
@@ -7,7 +7,7 @@ import {
   SET_MOST_RECENT_PROGRAM,
 } from "../actions/userDataActions";
 
-import { PYTHON } from "../constants";
+import { PYTHON, PROCESSING, HTML } from "../constants";
 
 import * as fetch from "../lib/fetch.js";
 
@@ -16,12 +16,13 @@ const initialState = {
   displayName: "",
   photoURL: null,
   uid: "",
-  mostRecentProgram: PYTHON,
+  mostRecentProgram: "",
 };
 
 function userDataReducer(state = initialState, action) {
   switch (action.type) {
     case LOAD_USER_DATA:
+      console.log(action.userData.mostRecentProgram);
       //pull all values we want to pay attention to out of the object
       return Object.assign({}, state, action.userData);
     case CLEAR_USER_DATA:
@@ -55,6 +56,17 @@ function userDataReducer(state = initialState, action) {
       //   })
       return state;
     case SET_MOST_RECENT_PROGRAM:
+      console.log(action.value);
+      fetch
+        .updateUserData(state.uid, { mostRecentProgram: action.value })
+        .then(response => {
+          console.log(action.value);
+          console.log(response);
+        })
+        .catch(err => {
+          state.error = err;
+          console.log(err);
+        });
       return Object.assign({}, state, { mostRecentProgram: action.value });
     default:
       return state;
