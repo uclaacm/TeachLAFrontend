7050e4ab1ead28b15f134af7181d786e527b56f7
Connor Borden <connorbo97@g.ucla.edu>
2019-05-26 14:54:49 -0700

    remove API key from repo, rename to src/firebase-config.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Root from "./components/containers/AppContainer.js";
 import registerServiceWorker from "./registerServiceWorker";
 import { createStore } from "redux";
 import appReducers from "./reducers";
-import config from "./firebase";
+import config from "./firebase-config.js";
 import { Provider } from "react-redux";
 import firebase from "firebase";
 
