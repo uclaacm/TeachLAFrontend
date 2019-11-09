08b10774e1df30492e3a46978a5a4afefd8d6b2f
krashanoff <lkrashanoff2@gmail.com>
2019-11-09 13:51:17 -0800

    Add ViewportAwareButton.

---
+++ src/components/Editor/components/ViewportAwareButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button } from "reactstrap";
+
+/**-------Props--------
+ * isSmall: dictates display of anything marked with className '.viewport-aware'
+ * ...    : any props you would pass to a reactstrap button
+ */
+const ViewportAwareButton = props => {
+  // eliminate viewport aware children if small.
+  let { children, ...remainder } = props;
+  if (props.isSmall)
+    children = Array.from(props.children).filter(
+      child => child.props.className !== "viewport-aware",
+    );
+
+  return <Button children={children} {...remainder} />;
+};
+
+export default ViewportAwareButton;
