eb01a0dfba4e8d3f819ffdd2922785f2f6433e80
krashanoff <lkrashanoff2@gmail.com>
2019-11-09 22:49:52 -0800

    Change ViewportAwareButton to self-closing tag.

--- src/components/Editor/components/ViewportAwareButton.js
+++ src/components/Editor/components/ViewportAwareButton.js
@@ -2,18 +2,25 @@ import React from "react";
 import { Button } from "reactstrap";
 
 /**-------Props--------
- * isSmall: dictates display of anything marked with className '.viewport-aware'
- * ...    : any props you would pass to a reactstrap button
+ * isSmall: dictates display of text field
+ * icon: desired icon (i.e. <FontAwesomeIcon icon={...} />)
+ * text: the text label for the button
+ */
+/**--------Optional props--------
+ * any props you would apply to a reactstrap Button.
  */
 const ViewportAwareButton = props => {
-  // eliminate viewport aware children if small.
-  let { children, ...remainder } = props;
-  if (props.isSmall)
-    children = Array.from(props.children).filter(
-      child => !child.props.className.split(" ").includes("viewport-aware"),
-    );
+  // extract icon and text from props.
+  let { icon, text, ...remainder } = props;
 
-  return <Button children={children} {...remainder} />;
+  // render icon always, text only if not small.
+  return (
+    <Button {...remainder}>
+      {icon}
+
+      {!props.isSmall && <span className="viewport-aware-button-text">&nbsp;&nbsp;{text}</span>}
+    </Button>
+  );
 };
 
 export default ViewportAwareButton;
