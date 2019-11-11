3620be147675e44b2253acedc8f5004fc2b682ba
Leonid <lkrashanoff2@gmail.com>
2019-11-10 19:37:59 -0800

    Add ViewportAwareButton to Editor. (#118)
    * Add ViewportAwareButton.
    
    * Add straightforward tests.
    
    * Finalize children filtering procedure. Add more tests.
    
    * Switch to ViewportAwareButtons in Editor components.
    
    * Add isSmall to Editor state.
    
    * Revert "Add isSmall to Editor state."
    Bad idea as would be confusing. Editor already has an isSmall prop.
    
    This reverts commit d3e223ad0d7c346252395f07472e67191d990862.
    
    * Change ViewportAwareButton to self-closing tag.
    
    * Update ViewportAwareButton tests for 9187f39.
    
    * Use updated ViewportAwareButtons in Editor.

---
+++ src/components/Editor/components/ViewportAwareButton.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import ViewportAwareButton from "./ViewportAwareButton";
+
+describe("<ViewportAwareButton />", () => {
+  it("renders all children when normal sized.", () => {
+    let component = shallow(<ViewportAwareButton isSmall={false} icon={<div />} text="hi" />);
+    expect(component.find("span") && component.find("div"));
+  });
+
+  it("doesn't render viewport-aware children when small.", () => {
+    let component = shallow(<ViewportAwareButton isSmall={true} icon={<div />} text="hi" />);
+    expect(!component.find("span") && component.find("div"));
+  });
+});
