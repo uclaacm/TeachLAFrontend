98e5df387f3ae728f5b7698eec31863b26303082
Jamie <jamie.liu386@gmail.com>
2019-11-14 14:56:01 -0800

    added buttons back to Output and Sketches

--- src/components/Output/Output.js
+++ src/components/Output/Output.js
@@ -4,7 +4,8 @@ import { OUTPUT_ONLY } from "../../constants";
 import EditorRadio from "../Editor/components/EditorRadio";
 import CreateProcessingDoc from "../Output/Processing";
 import CreatePythonDoc from "../Output/Python";
-
+import DropdownButtonContainer from "../common/containers/DropdownButtonContainer.js";
+import OpenPanelButtonContainer from "../common/containers/OpenPanelButtonContainer.js";
 import { Button } from "reactstrap";
 import ViewportAwareButton from "../common/ViewportAwareButton.js";
 
@@ -115,6 +116,10 @@ class Output extends React.Component {
     return this.renderIframe(srcDocFunc);
   };
 
+  renderOpenPanelButton = () => this.props.viewMode === OUTPUT_ONLY && <OpenPanelButtonContainer />;
+
+  renderLanguageDropdown = () => this.props.viewMode === OUTPUT_ONLY && <DropdownButtonContainer />;
+
   renderRadio = () =>
     this.props.viewMode === OUTPUT_ONLY && (
       <div style={{ marginLeft: "auto" }}>
@@ -146,6 +151,8 @@ class Output extends React.Component {
 
   renderBanner = () => (
     <div className="editor-output-banner">
+      {this.renderOpenPanelButton()}
+      {this.renderLanguageDropdown()}
       <div style={{ flex: "1 1 auto" }}> </div> {/*whitespace*/}
       {this.renderRadio()}
       {this.renderConsoleButton()}
--- src/components/Sketches/index.js
+++ src/components/Sketches/index.js
@@ -3,6 +3,7 @@ import SketchBox from "./components/SketchBox";
 import ConfirmDeleteModalContainer from "./containers/ConfirmDeleteModalContainer";
 import CreateSketchModalContainer from "./containers/CreateSketchModalContainer";
 import EditSketchModalContainer from "./containers/EditSketchModalContainer";
+import OpenPanelButtonContainer from "../common/containers/OpenPanelButtonContainer";
 import { SketchThumbnailArray } from "./constants";
 import ProcessingConstructor from "../Output/Processing";
 // import { PANEL_SIZE } from "../../constants";
@@ -88,6 +89,7 @@ class Sketches extends React.Component {
 
   renderHeader = () => (
     <div className="sketches-header">
+      <OpenPanelButtonContainer />
       <div className="sketches-header-text">Sketches</div>
       <Button
         className="ml-auto mr-2"
