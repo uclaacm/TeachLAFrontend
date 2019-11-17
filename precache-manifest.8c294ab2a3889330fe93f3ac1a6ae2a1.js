8c4019d76a02daf91382c719d4c869c76a45752b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-17 03:17:11 +0000

    Deploy uclaacm/TeachLAFrontend to github.com/uclaacm/TeachLAFrontend.git:gh-pages

---
+++ precache-manifest.8c294ab2a3889330fe93f3ac1a6ae2a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b224de07236cb3128002bd2443bea6fd",
+    "url": "/TeachLAFrontend/index.html"
+  },
+  {
+    "revision": "1a6e586b2465d8c970aa",
+    "url": "/TeachLAFrontend/static/css/2.bec2df5d.chunk.css"
+  },
+  {
+    "revision": "6a0e97989593ea4a68b3",
+    "url": "/TeachLAFrontend/static/css/main.629fe7c6.chunk.css"
+  },
+  {
+    "revision": "1a6e586b2465d8c970aa",
+    "url": "/TeachLAFrontend/static/js/2.a6af0b23.chunk.js"
+  },
+  {
+    "revision": "6a0e97989593ea4a68b3",
+    "url": "/TeachLAFrontend/static/js/main.498d852e.chunk.js"
+  },
+  {
+    "revision": "899b97e73d2959d7bcc2",
+    "url": "/TeachLAFrontend/static/js/runtime-main.91b3a2fc.js"
+  },
+  {
+    "revision": "ade51e631d9d9533a683bb2e06836096",
+    "url": "/TeachLAFrontend/static/media/blueguy.ade51e63.png"
+  },
+  {
+    "revision": "96efe073f1d3d4452d1d22a4360dab69",
+    "url": "/TeachLAFrontend/static/media/tla-logo.96efe073.svg"
+  }
+]);
