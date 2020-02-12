51c7178f036f5fd527447e16c06d70391b753aa2
evazhog <evanzhong@ucla.edu>
2020-02-11 20:26:36 -0800

    isolated ImageSelector scss classes

--- src/components/common/ImageSelector.js
+++ src/components/common/ImageSelector.js
@@ -1,23 +1,24 @@
 import React from "react";
 import ReactModal from "react-modal";
 import { Container } from "reactstrap";
+import "../../styles/ImageSelector.scss";
 class ImageSelector extends React.Component {
   render(){
     return(
       <ReactModal
         isOpen={this.props.isOpen}
         onRequestClose={this.props.closeModal}
-        className="sketches-image-modal"
+        className="image-selector-modal"
         overlayClassName="profile-image-overlay"
         ariaHideApp={false}
       >
         <Container style={{"max-width": `${this.props.maxWidth}px`,}}>
-          <div className="sketches-modal-header d-flex align-items-center">
+          <div className=".image-selector-modal-header d-flex align-items-center">
             <h1>Choose a thumbnail</h1>
-            <div className="sketches-modal-header-thumbnail-container">{this.props.thumbnailPreview || null}</div>
+            <div className="image-selector-modal-header-thumbnail-container">{this.props.thumbnailPreview || null}</div>
           </div>
           <hr />
-          <div className="sketches-gallery">{this.props.icons}</div>
+          <div className="image-selector-gallery">{this.props.icons}</div>
           <br />
           <div className="text-center text-danger">{this.props.error || <br />}</div>
           <hr />
