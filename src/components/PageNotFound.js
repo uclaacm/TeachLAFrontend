37d46867311fa5224ca2c3ce665bf4c2cdfd4ab9
Matthew Wang <matt@matthewwang.me>
2020-02-28 17:46:15 -0800

    properly implements cookie-getting on Error and 404
    even if cookie doesn't exist, there are sensible defaults

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -4,28 +4,31 @@ import Footer from "./common/Footer";
 import { Link } from "react-router-dom";
 import "styles/Page.scss";
 import { GH_REPO_NAME } from "../constants";
+import * as cookies from "../lib/cookies.js";
 
 class PageNotFound extends React.Component {
   render() {
     return (
-      <div className="page-container">
-        <span>Error: 404</span>
-        <h1>Uh oh, page not found!</h1>
-        <br />
-        <Link to="/">
-          <Button color="success" size="lg">
-            Back to safety
-          </Button>
-        </Link>
-        <br />
-        <p>
-          Getting this problem a lot? Let us know{" "}
-          <a target="_blank" rel="noopener noreferrer" href={`${GH_REPO_NAME}/issues`}>
-            on GitHub
-          </a>
-          .
-        </p>
-        <Footer />
+      <div className={`full-container theme-` + cookies.getThemeFromCookie()}>
+        <div className={`page-container`}>
+          <span>Error: 404</span>
+          <h1>Uh oh, page not found!</h1>
+          <br />
+          <Link to="/">
+            <Button color="success" size="lg">
+              Back to safety
+            </Button>
+          </Link>
+          <br />
+          <p>
+            Getting this problem a lot? Let us know{" "}
+            <a target="_blank" rel="noopener noreferrer" href={`${GH_REPO_NAME}/issues`}>
+              on GitHub
+            </a>
+            .
+          </p>
+          <Footer />
+        </div>
       </div>
     );
   }
