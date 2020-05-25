57a6f586dd08920ae058ac917b9a096583573869
Matt Wang <matt@matthewwang.me>
2020-05-25 01:54:54 -0700

    removes ref in index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,7 @@
 import * as cookies from "./cookies.js";
 import * as fetch from "./fetch.js";
-import * as helpers from "./fetch.js";
 
 export default {
   ...cookies,
   ...fetch,
-  ...helpers,
 };
