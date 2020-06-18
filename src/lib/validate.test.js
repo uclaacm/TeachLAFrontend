ccdc4a99cf4bf6a85f3b8267dc124a2063d26efa
Matt Wang <matt@matthewwang.me>
2020-06-17 21:34:36 -0700

    updating username and displayname to allow -_.

--- src/lib/validate.test.js
+++ src/lib/validate.test.js
@@ -10,16 +10,19 @@ import {
 import { isValidUsername, isValidPassword, isValidDisplayName } from "./validate";
 
 const usernameSize = `Username must be between ${MINIMUM_USERNAME_LENGTH}-${MAXIMUM_USERNAME_LENGTH} characters long`;
-const usernameBadCharacters = `Username must only use upper case letters, lower case letters, and numbers.`;
+const usernameBadCharacters = `Username must only use upper case letters, lower case letters, numbers, or the characters .-_`;
 
 const passwordSize = `Password must be between ${MINIMUM_PASSWORD_LENGTH}-${MAXIMUM_PASSWORD_LENGTH} characters long`;
 
 const displayNameSize = `Display name must be between ${MINIMUM_DISPLAY_NAME_LENGTH}-${MAXIMUM_DISPLAY_NAME_LENGTH} characters long`;
-const displayNameBadCharacters = `Display name must only use upper case and lower case letters, numbers, spaces, and/or the special characters !@#$%`;
+const displayNameBadCharacters = `Display name must only use upper case and lower case letters, numbers, spaces, and/or the special characters !@#$%.-_`;
 
 describe("isValidUsername", () => {
   it("accepts a reasonable username", () => {
     expect(isValidUsername("bobbytables").ok).toBe(true);
+  });
+  it("accepts a username with permitted special characters", () => {
+    expect(isValidUsername("bobby-_.").ok).toBe(true);
   });
   it("rejects spaces in a username, returns proper error message", () => {
     const result = isValidUsername("bobby tables");
@@ -27,7 +30,7 @@ describe("isValidUsername", () => {
     expect(result.message).toBe(usernameBadCharacters);
   });
   it("rejects non-permitted special characters in username, returns proper error message", () => {
-    const result = isValidUsername("&^)_([]{}\\");
+    const result = isValidUsername("&^)([]{}\\");
     expect(result.ok).toBe(false);
     expect(result.message).toBe(usernameBadCharacters);
 
@@ -92,10 +95,10 @@ describe("isValidDisplayName", () => {
     expect(isValidDisplayName("bobby tables").ok).toBe(true);
   });
   it("accepts a display name with permitted special characters", () => {
-    expect(isValidDisplayName("bobby!@#$%").ok).toBe(true);
+    expect(isValidDisplayName("bobby!@#$%-._").ok).toBe(true);
   });
   it("rejects non-permitted special characters in display name, returns proper error message", () => {
-    const result = isValidDisplayName("&^)_([]{}\\");
+    const result = isValidDisplayName("&^)([]{}\\");
     expect(result.ok).toBe(false);
     expect(result.message).toBe(displayNameBadCharacters);
   });
