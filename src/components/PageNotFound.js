f5057f811a1159c7016d0622b6711737b7b4cd90
John Ayling <johnayling@yahoo.com>
2021-10-12 13:04:53 -0500

    634 :: Refactor PageNotFound to a functional component. (#636)
    Co-authored-by: John Ayling <john.ayling@digintent.com>

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -6,32 +6,31 @@ import { GH_REPO_NAME } from '../constants';
 import * as cookies from '../lib/cookies.js';
 import Footer from './common/Footer';
 
-class PageNotFound extends React.Component {
-  render() {
-    return (
-      <div className={`full-container theme-${cookies.getThemeFromCookie()}`}>
-        <div className="page-container">
-          <span>Error: 404</span>
-          <h1>Uh oh, page not found!</h1>
-          <br />
-          <Link to="/">
-            <Button color="success" size="lg">
-              Back to safety
-            </Button>
-          </Link>
-          <br />
-          <p>
-            Getting this problem a lot? Let us know
-            {' '}
-            <a target="_blank" rel="noopener noreferrer" href={`${GH_REPO_NAME}/issues`}>
-              on GitHub
-            </a>
-            .
-          </p>
-          <Footer />
-        </div>
+function PageNotFound() {
+  return (
+    <div className={`full-container theme-${cookies.getThemeFromCookie()}`}>
+      <div className="page-container">
+        <span>Error: 404</span>
+        <h1>Uh oh, page not found!</h1>
+        <br />
+        <Link to="/">
+          <Button color="success" size="lg">
+            Back to safety
+          </Button>
+        </Link>
+        <br />
+        <p>
+          Getting this problem a lot? Let us know
+          {' '}
+          <a target="_blank" rel="noopener noreferrer" href={`${GH_REPO_NAME}/issues`}>
+            on GitHub
+          </a>
+          .
+        </p>
+        <Footer />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default PageNotFound;
