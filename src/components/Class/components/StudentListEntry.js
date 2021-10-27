010553177fd1e1f574876f6d68774be9e05936e7
Jack Lin <jacklin1229@gmail.com>
2021-10-26 22:49:14 -0700

    test1

--- src/components/Class/components/StudentListEntry.js
+++ src/components/Class/components/StudentListEntry.js
@@ -5,7 +5,7 @@ class StudentListEntry extends React.Component {
   render() {
     return (
       <div className="class-box class-box-body">
-        <div className="class-info">
+        <div className="clasdads-info">
           <b className="fa-lg">{this.props.name}</b>
           <div className="fa-lg">{this.props.name}</div>
         </div>
