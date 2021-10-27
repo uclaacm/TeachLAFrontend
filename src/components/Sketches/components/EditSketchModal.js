c135ff828f00e151f5adf618390f36222d62f9b9
Nathan Endow <35786272+nendow02@users.noreply.github.com>
2021-10-26 18:44:49 -0700

    Update EditSketchModal.js
    Refactored editSketchModal

--- src/components/Sketches/components/EditSketchModal.js
+++ src/components/Sketches/components/EditSketchModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactModal from 'react-modal';
 import { Button, Container, Row, Col, FormGroup, Label, Input } from 'reactstrap';
 
@@ -8,87 +8,75 @@ import { SketchThumbnailArray, LanguageDropdownValues } from '../constants';
 import DropdownButton from '../../common/DropdownButton.js';
 import '../../../styles/SketchesModal.scss';
 
-class EditSketchModal extends React.Component {
-  toggleProps = { className: '', color: 'primary', size: 'lg' };
+const EditSketchModal = (props) => {
+  const { isOpen, onClose, setProgramLanguage, setProgramName, setProgramThumbnail, sketchImg, sketchKey, sketchLang, sketchName} = props;
+  const toggleProps = { className: '', color: 'primary', size: 'lg' };
+  
+  const [newLanguage,setNewLanguage] = useState(-1);
+  const [newName,setNewName] = useState(-1);
+  const [newThumbnail, setNewThumbnail] = useState(-1);
+  const [disableSubmit,setDisableSubmit] = useState(false);
+  const [error,setError] = useState('');
+  const [onThumbnails,setOnThumbnails] = useState(false);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      newLanguage: -1,
-      newName: -1,
-      newThumbnail: -1,
-      disableSubmit: false,
-      error: '',
-      onThumbnails: false,
-    };
-  }
-
-  closeModal = () => {
-    if (this.props.onClose && {}.toString.call(this.props.onClose) === '[object Function]') {
-      this.props.onClose();
+  const closeModal = () => {
+    if (onClose && {}.toString.call(onClose) === '[object Function]') {
+      onClose();
     }
-    this.setState({
-      newLanguage: -1,
-      newName: -1,
-      newThumbnail: -1,
-      error: '',
-      onThumbnails: false,
-      disableSubmit: false,
-    });
   };
 
-  changeLanguage = (lang) => {
-    this.setState({ newLanguage: lang });
+  const changeLanguage = (lang) => {
+    setNewLanguage(lang);
   };
 
   // Next two bad____input are copied (and slightly modified) from create sketch modal
 
-  badNameInput = () => {
-    if (this.state.newName === -1) {
+  const badNameInput = () => {
+    if (newName === -1) {
       return false;
     }
-    if (!this.state.newName) {
-      this.setState({ error: 'Name is required' });
+    if (!newName) {
+      setError('Name is required');
       return true;
     }
-    if (this.state.newName.length > 15) {
-      this.setState({ error: 'Name must be 15 characters or less' });
+    if (newName.length > 15) {
+      setError('Name must be 15 characters or less');
       return true;
     }
     return false;
   };
 
-  badLanguageInput = () => {
-    if (this.state.newLanguage === -1) {
+  const badLanguageInput = () => {
+    if (newLanguage === -1) {
       return false;
     }
-    if (!this.state.newLanguage) {
-      this.setState({ error: 'Please select a language' });
+    if (newLanguage) {
+      setError('Please select a language' );
       return true;
     }
 
     let notFound = true;
     for (let i = 0; i < LanguageDropdownValues.length; i++) {
       if (
-        this.state.newLanguage.display === LanguageDropdownValues[i].display &&
-        this.state.newLanguage.value === LanguageDropdownValues[i].value
+        newLanguage.display === LanguageDropdownValues[i].display &&
+        newLanguage.value === LanguageDropdownValues[i].value
       ) {
         notFound = false;
         break;
       }
     }
     if (notFound) {
-      this.setState({ error: 'Invalid language selected' });
+      setError('Invalid language selected');
       return true;
     }
 
     return false;
   };
 
-  handleSubmitEdit = async (e) => {
+  const handleSubmitEdit = async (e) => {
     e.preventDefault();
 
-    if (this.badNameInput() || this.badLanguageInput()) {
+    if (badNameInput() || badLanguageInput()) {
       // note it's impossible to have a bad thumbnail input
       return;
     }
@@ -96,66 +84,63 @@ class EditSketchModal extends React.Component {
     const data = {};
     let doUpdate = false;
 
-    if (this.state.newLanguage !== -1) {
-      data.language = this.state.newLanguage.value;
+    if (newLanguage !== -1) {
+      data.language = newLanguage.value;
       doUpdate = true;
     }
-    if (this.state.newName !== -1) {
-      data.name = this.state.newName;
+    if (newName !== -1) {
+      data.name = newName;
       doUpdate = true;
     }
-    if (this.state.newThumbnail !== -1) {
-      data.thumbnail = this.state.newThumbnail;
+    if (newThumbnail !== -1) {
+      data.thumbnail = newThumbnail;
       doUpdate = true;
     }
     if (doUpdate) {
       const updateData = {};
-      updateData[this.props.sketchKey] = data;
+      updateData[sketchKey] = data;
       try {
         fetch
-          .updatePrograms(this.props.uid, updateData)
+          .updatePrograms(uid, updateData)
           .then((res) => {
             if (res.ok) {
-              if (this.state.newLanguage !== -1) {
-                this.props.setProgramLanguage(this.props.sketchKey, this.state.newLanguage.value);
+              if (newLanguage !== -1) {
+                setProgramLanguage(sketchKey, newLanguage.value);
               }
-              if (this.state.newName !== -1) {
-                this.props.setProgramName(this.props.sketchKey, this.state.newName);
+              if (newName !== -1) {
+                setProgramName(sketchKey, newName);
               }
-              if (this.state.newThumbnail !== -1) {
-                this.props.setProgramThumbnail(this.props.sketchKey, this.state.newThumbnail);
+              if (newThumbnail !== -1) {
+                setProgramThumbnail(sketchKey, newThumbnail);
               }
-              this.closeModal();
+              closeModal();
             } else {
-              this.setState({
-                disableSubmit: false,
-                error: res.text() || 'Failed to edit sketch, please try again later',
-              });
+              setDisableSubmit(false);
+              setError(res.text() || 'Failed to edit sketch, please try again later');
             }
           })
           .catch((err) => {
-            this.setState({
-              disableSubmit: false,
-              error: 'Failed to edit sketch, please try again later',
-            });
+            setDisableSubmit(false);
+            setError( 'Failed to edit sketch, please try again later');
             console.log(err);
           });
       } catch (err) {
         console.log(err);
       }
-      this.setState({ disableSubmit: true, error: '' });
+      setDisableSubmit(true);
+      setError('');
     } else {
-      this.closeModal();
+      closeModal();
     }
   };
 
-  renderMainModal() {
+  const renderMainModal = () => {
     const thumbnailPreview = (
       <img
         src={`${process.env.PUBLIC_URL}/img/sketch-thumbnails/${
-          this.state.newThumbnail !== -1
-            ? SketchThumbnailArray[this.state.newThumbnail]
-            : this.props.sketchImg
+          newThumbnail !== -1
+            ? SketchThumbnailArray[newThumbnail]
+            : sketchImg
         }.svg`}
         className="sketches-modal-header-thumbnail"
         alt="icon"
@@ -163,8 +148,8 @@ class EditSketchModal extends React.Component {
     );
     return (
       <ReactModal
-        isOpen={this.props.isOpen}
-        onRequestClose={this.closeModal}
+        isOpen={isOpen}
+        onRequestClose={closeModal}
         className="sketches-modal"
         overlayClassName="profile-image-overlay"
         ariaHideApp={false}
@@ -172,7 +157,7 @@ class EditSketchModal extends React.Component {
         <Container>
           <h2 className="text-center">
             Editing &quot;
-            {this.props.sketchName}
+            {sketchName}
             &quot;
           </h2>
           <hr />
@@ -182,8 +167,8 @@ class EditSketchModal extends React.Component {
             </Label>
             <Col xs={8}>
               <Input
-                onChange={(e) => this.setState({ newName: e.target.value })}
-                value={this.state.newName !== -1 ? this.state.newName : this.props.sketchName}
+                onChange={(e) => setNewName(e.target.value )}
+                value={newName !== -1 ? newName : sketchName}
                 id="sketch-name"
               />
             </Col>
@@ -196,13 +181,13 @@ class EditSketchModal extends React.Component {
             <Col xs="8" className="d-flex align-items-center">
               <DropdownButton
                 displayValue={
-                  this.state.newLanguage !== -1
-                    ? this.state.newLanguage.display
-                    : this.props.sketchLang
+                  newLanguage !== -1
+                    ? newLanguage.display
+                    : sketchLang
                 }
                 displayClass={'sketches'}
-                toggleProps={this.toggleProps}
-                onSelect={this.changeLanguage}
+                toggleProps={toggleProps}
+                onSelect={changeLanguage}
                 DropdownItems={LanguageDropdownValues}
               />
             </Col>
@@ -222,7 +207,7 @@ class EditSketchModal extends React.Component {
                 color="primary"
                 size="lg"
                 onClick={() => {
-                  this.setState({ onThumbnails: true });
+                  setOnThumbnails(true);
                 }}
               >
                 Change
@@ -230,15 +215,15 @@ class EditSketchModal extends React.Component {
             </Col>
           </Row>
           <br />
-          <div className="text-center text-danger">{this.state.error || <br />}</div>
+          <div className="text-center text-danger">{error || <br />}</div>
           <hr />
           <Row>
             <Col>
               <Button
                 color="danger"
-                onClick={this.closeModal}
+                onClick={closeModal}
                 size="lg"
-                disabled={this.state.disableSubmit}
+                disabled={disableSubmit}
                 block
               >
                 Cancel
@@ -247,9 +232,9 @@ class EditSketchModal extends React.Component {
             <Col>
               <Button
                 color="success"
-                onClick={this.handleSubmitEdit}
+                onClick={handleSubmitEdit}
                 size="lg"
-                disabled={this.state.disableSubmit}
+                disabled={disableSubmit}
                 block
               >
                 Confirm
@@ -261,26 +246,26 @@ class EditSketchModal extends React.Component {
     );
   }
 
-  renderThumbnailModal() {
+  const renderThumbnailModal = () => {
     const icons = SketchThumbnailArray.map((val, index) => (
       <figure
         className="sketches-gallery-item"
         key={val}
-        onClick={() => this.setState({ newThumbnail: index })}
+        onClick={() => setNewThumbnail(index)}
       >
         <img
           src={`${process.env.PUBLIC_URL}/img/sketch-thumbnails/${val}.svg`}
-          className={`sketches-gallery-img${this.state.newThumbnail === index ? '-selected' : ''}`}
+          className={`sketches-gallery-img${newThumbnail === index ? '-selected' : ''}`}
           alt="icon"
         />
       </figure>
     ));
 
     const thumbnailPreview =
-      this.state.newThumbnail !== -1 ? (
+      newThumbnail !== -1 ? (
         <img
           src={`${process.env.PUBLIC_URL}/img/sketch-thumbnails/${
-            SketchThumbnailArray[this.state.newThumbnail]
+            SketchThumbnailArray[newThumbnail]
           }.svg`}
           className="sketches-modal-header-thumbnail"
           alt="icon"
@@ -294,18 +279,18 @@ class EditSketchModal extends React.Component {
       );
     return (
       <ImageSelector
-        isOpen={this.props.isOpen}
-        closeModal={this.closeModal}
+        isOpen={isOpen}
+        closeModal={closeModal}
         thumbnailPreview={thumbnailPreview}
         icons={icons}
-        error={this.state.error}
+        error={error}
       >
         <Row>
           <Col>
             <Button
               color="secondary"
               onClick={() => {
-                this.setState({ onThumbnails: false });
+                setOnThumbnails(false);
               }}
               size="lg"
               block
@@ -317,14 +302,11 @@ class EditSketchModal extends React.Component {
       </ImageSelector>
     );
   }
-
-  render() {
-    if (this.state.onThumbnails) {
-      return this.renderThumbnailModal();
-    }
-
-    return this.renderMainModal();
+  if (onThumbnails) {
+    return renderThumbnailModal();
   }
+
+  return renderMainModal();
 }
 
 export default EditSketchModal;
