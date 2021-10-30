fcfa1af00c47039c91087f8947291d6c664e0af6
Megan Go <Meganggo11@gmail.com>
2021-10-29 19:27:44 -0700

    fixed merge conflict
    - render content
    - lint conflicts resolved

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 
 import { EDITOR_WIDTH_BREAKPOINT, CODE_AND_OUTPUT, CODE_ONLY } from '../constants';
-import * as fetch from "../lib/fetch.js";
-import * as cookies from "../lib/cookies.js";
-import ClassPageContainer from "./Class/containers/ClassPageContainer";
-import ClassesPageContainer from "./Classes/containers/ClassesContainer";
-import ProfilePanelContainer from "./common/containers/ProfilePanelContainer";
-import SketchesPageContainer from "./Sketches/containers/SketchesContainer";
-import EditorAndOutput from "./EditorAndOutput/EditorAndOutput";
+import * as cookies from '../lib/cookies';
+import * as fetch from '../lib/fetch';
+import ClassPageContainer from './Class/containers/ClassPageContainer';
+import ClassesPageContainer from './Classes/containers/ClassesContainer';
+import ProfilePanelContainer from './common/containers/ProfilePanelContainer';
+import EditorAndOutput from './EditorAndOutput/EditorAndOutput';
+import SketchesPageContainer from './Sketches/containers/SketchesContainer';
 
 import '../styles/Main.scss';
 
@@ -19,18 +19,33 @@ import '../styles/Main.scss';
  */
 
 const Main = ({
-  screenWidth, theme, dirty, mostRecentProgram, code, uid,
-  contentType, left, screenHeight, panelOpen, language, programid,
-  sketchName, listOfPrograms, setTheme
+  screenWidth,
+  theme,
+  dirty,
+  mostRecentProgram,
+  code,
+  uid,
+  contentType,
+  left,
+  screenHeight,
+  panelOpen,
+  language,
+  programid,
+  sketchName,
+  listOfPrograms,
+  setTheme,
+  cleanCode,
 }) => {
   const [saveText, setSaveText] = useState('Save');
-  const [viewMode, setViewMode] = useState(screenWidth <= EDITOR_WIDTH_BREAKPOINT ? CODE_ONLY : CODE_AND_OUTPUT);
+  const [viewMode, setViewMode] = useState(
+    screenWidth <= EDITOR_WIDTH_BREAKPOINT ? CODE_ONLY : CODE_AND_OUTPUT,
+  );
   const [pane1Style, setPane1Style] = useState({ transition: 'width .5s ease' });
 
   // Set theme from cookies (yum)
   useEffect(() => {
     setTheme(cookies.getThemeFromCookie());
-  },[]);
+  }, []);
 
   useEffect(() => {
     if (screenWidth <= EDITOR_WIDTH_BREAKPOINT) {
@@ -38,17 +53,17 @@ const Main = ({
         setViewMode(CODE_ONLY);
       }
     }
-  },[screenWidth]);
+  }, [screenWidth]);
 
   const onThemeChange = () => {
     const newTheme = theme === 'dark' ? 'light' : 'dark';
     cookies.setThemeCookie(newTheme);
     setTheme(newTheme);
-  }
+  };
 
   const resetSaveText = () => {
     setSaveText('Save');
-  }
+  };
 
   const handleSave = () => {
     if (!dirty) return; // Don't save if not dirty (unedited)
@@ -56,8 +71,8 @@ const Main = ({
 
     const programToUpdate = {};
     programToUpdate[mostRecentProgram] = {
-      code: code,
-    }
+      code,
+    };
 
     fetch.updatePrograms(uid, programToUpdate).then(() => {
       setSaveText('Saved!');
@@ -65,23 +80,13 @@ const Main = ({
       setTimeout(resetSaveText, 3000);
     });
     cleanCode(mostRecentProgram); // Set code's "dirty" state to false
-  }
-
-  const renderContent = () => {
-    switch (contentType) {
-    case 'editor':
-      return renderEditor();
-    case 'sketches':
-    default:
-      return <SketchesPageContainer />;
-    }
-  }
+  };
 
   const renderEditor = () => (
     <EditorAndOutput
       // view mode
       viewMode={viewMode}
-      updateViewMode={(viewMode) => setViewMode(viewMode)}
+      updateViewMode={(mode) => setViewMode(mode)}
       // theme
       theme={theme}
       // sizing
@@ -106,27 +111,41 @@ const Main = ({
     />
   );
 
-    // this stops us from rendering editor with no sketches available
-    if (contentType === 'editor' && listOfPrograms.length === 0) {
-      return <Redirect to="/sketches" />;
+  const renderContent = () => {
+    switch (contentType) {
+    case 'editor':
+      return renderEditor();
+    case 'classes':
+      return <ClassesPageContainer />;
+    case 'classPage':
+      return <ClassPageContainer />;
+    case 'sketches':
+    default:
+      return <SketchesPageContainer />;
     }
-    const codeStyle = {
-      left: left || 0,
-      width: screenWidth - (left || 0),
-      height: screenHeight,
-    }
+  };
 
-    return (
-      <div className={`main theme-${theme}`}>
-        <ProfilePanelContainer
-          contentType={contentType}
-          theme={theme}
-          onThemeChange={onThemeChange}
-        />
-        <div className="editor" style={codeStyle}>
-          {renderContent()}
-        </div>
+  // this stops us from rendering editor with no sketches available
+  if (contentType === 'editor' && listOfPrograms.length === 0) {
+    return <Redirect to="/sketches" />;
+  }
+  const codeStyle = {
+    left: left || 0,
+    width: screenWidth - (left || 0),
+    height: screenHeight,
+  };
+
+  return (
+    <div className={`main theme-${theme}`}>
+      <ProfilePanelContainer
+        contentType={contentType}
+        theme={theme}
+        onThemeChange={onThemeChange}
+      />
+      <div className="editor" style={codeStyle}>
+        {renderContent()}
       </div>
-    );
-}
+    </div>
+  );
+};
 export default Main;
