9a2fc4685f138a71c7bd2759d057649bcbb6c2b4
peacebonez <kmpariso12@gmail.com>
2021-10-31 12:01:45 -0400

    achieve 0 linting errors

--- src/components/Classes/components/ClassBox.js
+++ src/components/Classes/components/ClassBox.js
@@ -1,40 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "styles/ClassBox.scss";
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../../styles/ClassBox.scss';
 
-import { Row, Col } from "reactstrap";
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const ClassBox = ({
+  deleteFunc, img, instructorString, name, redirFunc, showLeaveButton,
+}) => {
+  const leaveButton = showLeaveButton ? (
+    <div
+      className="p-2 text-center"
+      onClick={deleteFunc}
+      onKeyDown={deleteFunc}
+      role="button"
+      tabIndex={0}
+    >
+      <FontAwesomeIcon className="fa-lg" icon={faTrashAlt} />
+    </div>
+  ) : (
+    ''
+  );
 
-class ClassBox extends React.Component {
-  render() {
-    let leaveButton = this.props.showLeaveButton ? (
-      <div className="p-2 text-center" onClick={this.props.deleteFunc}>
-        <FontAwesomeIcon className="fa-lg" icon={faTrashAlt} />
-      </div>
-    ) : (
-      ""
-    );
-
-    return (
-      <div className="class-box">
-        <Link className="class-box-body" onClick={this.props.redirFunc} to={{ pathname: "/class" }}>
-          <img
-            alt={"Class icon"}
-            src={`${process.env.PUBLIC_URL}/img/sketch-thumbnails/${this.props.img}.svg`}
-            className="class-thumbnail"
-          />
-          <div className="class-info">
-            <b className="fa-lg">{this.props.name}</b>
-            <div className="fa-lg instructor-name">
-              {this.props.instructorString}
-            </div>
-          </div>
-        </Link>
-        {leaveButton}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="class-box">
+      <Link className="class-box-body" onClick={redirFunc} to={{ pathname: '/class' }}>
+        <img
+          alt="Class icon"
+          src={`${process.env.PUBLIC_URL}/img/sketch-thumbnails/${img}.svg`}
+          className="class-thumbnail"
+        />
+        <div className="class-info">
+          <b className="fa-lg">{name}</b>
+          <div className="fa-lg instructor-name">{instructorString}</div>
+        </div>
+      </Link>
+      {leaveButton}
+    </div>
+  );
+};
 
 export default ClassBox;
