f980a6ea2b09f327f2c2c0f80b254ca09e6bd58c
Jacob Zhi <jacob@s-169-232-220-225.resnet.ucla.edu>
2021-11-16 21:41:41 -0800

    Added tests and snapshots for DropdownButton

---
+++ src/components/common/DropdownButton.test.js
@@ -0,0 +1,129 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import { DropdownToggle, DropdownItem } from 'reactstrap';
+import DropdownButton from './DropdownButton';
+
+const validDropdownItems = [
+  {
+    display: 'Nice',
+    value: 'banana',
+    icon: 'cubes',
+  },
+  {
+    display: 'Cool',
+    value: 'apple',
+    icon: 'mobile',
+  },
+  {
+    display: 'Fun',
+    value: 'pear',
+    icon: 'wind',
+  },
+];
+
+describe('DropdownButton', () => {
+  it('smoke test', () => {
+    const component = shallow(<DropdownButton DropdownItems={validDropdownItems} />);
+    expect(component.exists()).toBe(true);
+  });
+
+  it('renders every dropdown item correctly', () => {
+    const component = shallow(
+      <DropdownButton
+        icon="bacteria"
+        DropdownItems={validDropdownItems}
+        defaultOpen
+        displayValue="bar"
+      />,
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  it('handles defaultOpen properly', () => {
+    const componentTrue = shallow(
+      <DropdownButton
+        icon="lungs"
+        DropdownItems={validDropdownItems}
+        defaultOpen
+        displayValue="foo"
+      />,
+    );
+    expect(componentTrue).toMatchSnapshot();
+
+    const componentDefault = shallow(
+      <DropdownButton icon="lungs" DropdownItems={validDropdownItems} displayValue="foo" />,
+    );
+    expect(componentDefault).toMatchSnapshot();
+
+    const componentFalse = shallow(
+      <DropdownButton
+        icon="lungs"
+        DropdownItems={validDropdownItems}
+        defaultOpen={false}
+        displayValue="foo"
+      />,
+    );
+    expect(componentFalse).toMatchSnapshot();
+  });
+
+  it('handles dropdown click correctly', () => {
+    const componentOpen = shallow(
+      <DropdownButton icon="lungs" DropdownItems={validDropdownItems} displayValue="foo" />,
+    );
+    componentOpen.find(DropdownToggle).simulate('click');
+    expect(componentOpen).toMatchSnapshot();
+
+    const componentClose = shallow(
+      <DropdownButton
+        icon="lungs"
+        defaultOpen
+        DropdownItems={validDropdownItems}
+        displayValue="foo"
+      />,
+    );
+    componentClose.find(DropdownToggle).simulate('click');
+    expect(componentClose).toMatchSnapshot();
+  });
+
+  it('clicking an option triggers the handleClick function', () => {
+    const clickFn = jest.fn(({ display, value, icon }) => ({ display, value, icon }));
+
+    // check that clicking the selected value calls the handleClick fn
+
+    const component = shallow(
+      <DropdownButton
+        icon="lungs"
+        DropdownItems={validDropdownItems}
+        defaultOpen
+        displayValue="foo"
+        displayClass="bar"
+        onSelect={clickFn}
+      />,
+    );
+
+    component.find(DropdownItem).at(0).simulate('click');
+    expect(clickFn.mock.calls[0][0]).toStrictEqual({
+      dirty: undefined,
+      display: 'Nice',
+      value: 'banana',
+    });
+
+    const component2 = shallow(
+      <DropdownButton
+        icon="bacteria"
+        DropdownItems={validDropdownItems}
+        defaultOpen
+        displayValue="foo"
+        displayClass="bar"
+        onSelect={clickFn}
+      />,
+    );
+
+    component2.find(DropdownItem).at(1).simulate('click');
+    expect(clickFn.mock.calls[1][0]).toStrictEqual({
+      dirty: undefined,
+      display: 'Cool',
+      value: 'apple',
+    });
+  });
+});
