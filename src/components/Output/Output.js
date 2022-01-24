a11ad5bbaec59967a79a183f79d101bc4c6da4e8
Premkumar Giridhar <premgiridhar11@gmail.com>
2022-01-23 16:39:35 -0800

    Refactored Output.js component to avoid using an <iframe> when running Python code (#759)
    Made the Skulpt library draw directly to the canvas when running Python turtle code and write directly to the output container element when printing to the console. The code still uses <iframe> for the others (namely React and Processing.js), but I haven't been able to find a suitable online editor for those from which to draw inspiration.

--- src/components/Output/Output.js
+++ src/components/Output/Output.js
@@ -4,9 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Button } from 'reactstrap';
 import { OUTPUT_ONLY } from '../../constants';
-import OpenPanelButtonContainer from '../common/containers/OpenPanelButtonContainer.js';
-import ViewportAwareButton from '../common/ViewportAwareButton.js';
-import EditorRadio from '../TextEditor/components/EditorRadio.js';
+import OpenPanelButtonContainer from '../common/containers/OpenPanelButtonContainer';
+import ViewportAwareButton from '../common/ViewportAwareButton';
+import EditorRadio from '../TextEditor/components/EditorRadio';
+import '../../styles/Output.scss';
 
 /** --------Props--------
  * None
@@ -26,23 +27,25 @@ class Output extends React.Component {
 
   //= =============React Lifecycle Functions===================//
   shouldComponentUpdate(nextProps, nextState) {
-    if (this.state.showConsole !== nextState.showConsole) {
+    const { showConsole, run, counter } = this.state;
+    const { mostRecentProgram, isSmall } = this.props;
+    if (showConsole !== nextState.showConsole) {
       return true;
     }
 
-    if (this.props.mostRecentProgram !== nextProps.mostRecentProgram) {
+    if (mostRecentProgram !== nextProps.mostRecentProgram) {
       this.firstLoad = true;
       return true;
     }
 
-    if (this.props.isSmall !== nextProps.isSmall) {
+    if (isSmall !== nextProps.isSmall) {
       return true;
     }
 
     if (
-      this.state.run !== nextState.run
-      || this.state.counter !== nextState.counter
-      || this.state.showConsole !== nextState.showConsole
+      run !== nextState.run
+      || counter !== nextState.counter
+      || showConsole !== nextState.showConsole
     ) {
       this.firstLoad = false;
       return true;
@@ -50,32 +53,40 @@ class Output extends React.Component {
     return false;
   }
 
-  renderOpenPanelButton = () => this.props.viewMode === OUTPUT_ONLY && <OpenPanelButtonContainer />;
+  componentDidUpdate() {
+    this.updateOutput();
+  }
 
-  renderIframe = (getSrcDoc) => {
-    // check if getsrcdoc is a function
-    if (!getSrcDoc && {}.toString.call(getSrcDoc) === '[object Function]') {
-      console.log('Null src doc function found');
+  updateOutput = () => {
+    const {
+      viewOnly, vLanguage, language, code, runResult,
+    } = this.props;
+
+    const renderLanguage = viewOnly ? vLanguage : language;
+    const renderRunResult = viewOnly ? code : runResult;
+    const { showConsole } = this.state;
+
+    // if there's nothing to run, don't render an output
+    if (!runResult || !runResult.length) {
       return null;
     }
 
-    return (
-      <iframe
-        id={`${this.state.counter} ${this.state.run}`}
-        key={`${this.state.counter} ${this.state.run}`}
-        className="editor-output-iframe"
-        style={{ height: `${this.props.screenHeight - 61}px` }}
-        srcDoc={getSrcDoc()}
-        src=""
-        title="output-iframe"
-        onLoad={() => {}}
-      />
-    );
+    const srcDocFunc = () => renderLanguage.render(renderRunResult, showConsole);
+    return this.renderIframe(srcDocFunc);
+  };
+
+  runCode = () => {
+    this.setState((prevState) => ({
+      run: prevState.run + 1,
+    }));
   };
 
   renderOutput = () => {
-    const language = this.props.viewOnly ? this.props.vLanguage : this.props.language;
-    const runResult = this.props.viewOnly ? this.props.code : this.props.runResult;
+    const {
+      viewOnly, vLanguage, language, code, runResult,
+    } = this.props;
+    const renderLanguage = viewOnly ? vLanguage : language;
+    const renderRunResult = viewOnly ? code : runResult;
     const { showConsole } = this.state;
 
     if (this.firstLoad) {
@@ -87,63 +98,113 @@ class Output extends React.Component {
       return null;
     }
 
-    const srcDocFunc = () => language.render(runResult, showConsole);
+    const srcDocFunc = () => renderLanguage.render(renderRunResult, showConsole);
     return this.renderIframe(srcDocFunc);
   };
 
-  renderRadio = () => this.props.viewMode === OUTPUT_ONLY && (
-    <div style={{ marginLeft: 'auto' }}>
-      <EditorRadio
-        viewMode={this.props.viewMode}
-        updateViewMode={this.props.updateViewMode}
-        isSmall={this.props.isSmall}
+  renderOpenPanelButton = () => {
+    const { viewMode } = this.props;
+    return viewMode === OUTPUT_ONLY && <OpenPanelButtonContainer />;
+  };
+
+  renderIframe = (getSrcDoc) => {
+    const { counter, run } = this.state;
+    const { screenHeight } = this.props;
+
+    // check if getsrcdoc is a function
+    if (!getSrcDoc && {}.toString.call(getSrcDoc) === '[object Function]') {
+      return null;
+    }
+
+    return (
+      <iframe
+        id={`${counter} ${run}`}
+        key={`${counter} ${run}`}
+        className="editor-output-iframe"
+        style={{ height: `${screenHeight - 61}px` }}
+        srcDoc={getSrcDoc()}
+        src=""
+        title="output-iframe"
+        onLoad={() => {}}
       />
-    </div>
-  );
+    );
+  };
+
+  renderRadio = () => {
+    const { viewMode, updateViewMode, isSmall } = this.props;
+    return (
+      viewMode === OUTPUT_ONLY && (
+        <div style={{ marginLeft: 'auto' }}>
+          <EditorRadio viewMode={viewMode} updateViewMode={updateViewMode} isSmall={isSmall} />
+        </div>
+      )
+    );
+  };
 
   toggleConsole = () => {
     this.setState((prevState) => ({ showConsole: !prevState.showConsole }));
   };
 
-  renderConsoleButton = () => (
-    <Button
-      className="mx-2"
-      color={this.state.showConsole ? 'danger' : 'primary'}
-      onClick={this.toggleConsole}
-      title={this.state.showConsole ? 'Hide Console' : 'Show Console'}
-      size="lg"
-    >
-      <FontAwesomeIcon icon={faTerminal} />
-    </Button>
-  );
-
-  renderBanner = () => (
-    <div className="editor-output-banner">
-      {this.renderOpenPanelButton()}
-      <div style={{ flex: '1 1 auto' }}> </div>
-      {' '}
-      {/* whitespace */}
-      {this.renderRadio()}
-      {this.renderConsoleButton()}
-      <ViewportAwareButton
+  renderConsoleButton = () => {
+    const { showConsole } = this.state;
+    return (
+      <Button
         className="mx-2"
-        color="primary"
+        color={showConsole ? 'danger' : 'primary'}
+        onClick={this.toggleConsole}
+        title={showConsole ? 'Hide Console' : 'Show Console'}
         size="lg"
-        onClick={this.runCode}
-        isSmall={this.props.isSmall}
-        icon={<FontAwesomeIcon icon={faPlay} />}
-        text="Run"
-      />
-    </div>
-  );
+      >
+        <FontAwesomeIcon icon={faTerminal} />
+      </Button>
+    );
+  };
 
-  runCode = () => {
-    this.setState((prevState) => ({
-      run: prevState.run + 1,
-    }));
+  renderBanner = () => {
+    const { isSmall } = this.props;
+    return (
+      <div className="editor-output-banner">
+        {this.renderOpenPanelButton()}
+        <div style={{ flex: '1 1 auto' }}> </div>
+        {' '}
+        {/* whitespace */}
+        {this.renderRadio()}
+        {this.renderConsoleButton()}
+        <ViewportAwareButton
+          className="mx-2"
+          color="primary"
+          size="lg"
+          onClick={this.runCode}
+          isSmall={isSmall}
+          icon={<FontAwesomeIcon icon={faPlay} />}
+          text="Run"
+        />
+      </div>
+    );
   };
 
   render() {
+    const {
+      viewOnly, vLanguage, language, screenHeight,
+    } = this.props;
+    const { counter, run } = this.state;
+    const renderLanguage = viewOnly ? vLanguage : language;
+    if (renderLanguage.value === 'python') {
+      return (
+        <div className="editor-output">
+          {this.renderBanner()}
+          <div
+            id={`${counter} ${run}`}
+            key={`${counter} ${run}`}
+            className="editor-output-iframe"
+            style={{ height: `${screenHeight - 61}px` }}
+          >
+            <textarea id="inner" readOnly />
+            <div id="my-canvas" />
+          </div>
+        </div>
+      );
+    }
     return (
       <div className="editor-output">
         {this.renderBanner()}
