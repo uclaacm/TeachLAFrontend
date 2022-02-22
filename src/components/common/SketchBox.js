47ff045e95b7377fc4c41a285c7c651aae6b9ca5
Colbert Xuan <xuancc@g.ucla.edu>
2022-02-22 13:25:26 -0800

    refactored SketchBox.js to a functional component

--- src/components/common/SketchBox.js
+++ src/components/common/SketchBox.js
@@ -6,54 +6,66 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col } from 'reactstrap';
 
-class SketchBox extends React.Component {
-  render() {
-    const buttonData = [
-      {
-        func: this.props.editFunc,
-        icon: faEdit,
-      },
-      {
-        func: this.props.downloadFunc,
-        icon: faDownload,
-      },
-      {
-        func: this.props.deleteFunc,
-        icon: faTrashAlt,
-      },
-    ];
-    return (
-      <div className="sketch-box">
-        <Link
-          className="sketch-box-body"
-          onClick={this.props.redirFunc}
-          to={{ pathname: this.props.pathname || '/editor' }}
-        >
-          <img
-            alt={"User's sketch icon"}
-            src={`${process.env.PUBLIC_URL}/img/sketch-thumbnails/${this.props.img}.svg`}
-            className="sketch-thumbnail mt-2"
-          />
-          <div className="sketch-metadata">
-            <b className="sketch-name">{this.props.name}</b>
-            <FontAwesomeIcon className="sketch-icon" icon={this.props.icon} />
-          </div>
-        </Link>
-        <hr className="sketch-divider" />
-        <Row className="sketch-box-body">
-          {
-            buttonData.filter((data) => data.func)
-              .map((data, idx) => (
-                <Col className="p-2 text-center" onClick={data.func} key={idx}>
-                  <FontAwesomeIcon className="fa-lg" icon={data.icon} />
-                </Col>
-              ))
-              .reduce((acc, curr) => (acc.length > 0 ? [...acc, (<div className="sketch-button-divider" />), curr] : [curr]), [])
-          }
-        </Row>
-      </div>
-    );
-  }
-}
+const SketchBox = function ({
+  img,
+  icon,
+  name,
+  key,
+  deleteFunc,
+  downloadFunc,
+  editFunc,
+  redirFunc,
+  pathname,
+}) {
+  const buttonData = [
+    {
+      func: editFunc,
+      icon: faEdit,
+    },
+    {
+      func: downloadFunc,
+      icon: faDownload,
+    },
+    {
+      func: deleteFunc,
+      icon: faTrashAlt,
+    },
+  ];
+
+  return (
+    <div className="sketch-box">
+      <Link
+        className="sketch-box-body"
+        onClick={redirFunc}
+        to={{ pathname: pathname || '/editor' }}
+      >
+        <img
+          alt={"User's sketch icon"}
+          src={`${process.env.PUBLIC_URL}/img/sketch-thumbnails/${img}.svg`}
+          className="sketch-thumbnail mt-2"
+        />
+        <div className="sketch-metadata">
+          <b className="sketch-name">{name}</b>
+          <FontAwesomeIcon className="sketch-icon" icon={icon} />
+        </div>
+      </Link>
+      <hr className="sketch-divider" />
+      <Row className="sketch-box-body">
+        {buttonData
+          .filter((data) => data.func)
+          .map((data, idx) => (
+            <Col className="p-2 text-center" onClick={data.func} key={idx}>
+              <FontAwesomeIcon className="fa-lg" icon={data.icon} />
+            </Col>
+          ))
+          .reduce(
+            (acc, curr) =>
+              acc.length > 0 ? [...acc, <div className="sketch-button-divider" />, curr] : [curr],
+            [],
+          )}
+      </Row>
+    </div>
+  );
+};
 
 export default SketchBox;
