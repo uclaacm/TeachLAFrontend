1270dd33550838e5cf3313e962bb4976c45fa5b2
Timothy Poon <timothy.poon520@gmail.com>
2022-05-04 16:37:37 -0700

    address comments

--- src/components/TextEditor/components/ShareSketchModal.js
+++ src/components/TextEditor/components/ShareSketchModal.js
@@ -11,6 +11,9 @@ import '../../../styles/Modals.scss';
  * ShareSketchModal is a full-screen modal that displays
  * the current program's view-only route, which the user can manually
  * copy or use the "Copy To Clipboard" Button
+ * @param {Boolean} showModal modal render state
+ * @param {Function} toggleModal function that toggles the modal's render state
+ * @param {String} shareUrl the URL to display/copy to clipboard
  * showModal modal render state
  *  toggleModal function that toggles the modal's render state
  *  shareUrl the URL to display/copy to clipboard
@@ -18,17 +21,17 @@ import '../../../styles/Modals.scss';
 
 function ShareSketchModal(props) {
   const { shareUrl, showModal, toggleModal } = props;
-  const [str, setStr] = useState([{ copyStatus: 'Hit "Copy to Clipboard"!' }]);
+  const [copyStatus, setCopyStatus] = useState('Hit "Copy to Clipboard"!');
 
   const initiateCopy = () => {
     navigator.clipboard.writeText(shareUrl).then(
       () => {
         // success
-        setStr({ copyStatus: 'Successfully copied!' });
+        setCopyStatus('Successfully copied!');
       },
       () => {
         // failed
-        setStr({ copyStatus: 'Copy failed. If this keeps on happening, let us know!' });
+        setCopyStatus('Copy failed. If this keeps on happening, let us know!');
       },
     );
   };
@@ -43,15 +46,15 @@ function ShareSketchModal(props) {
     >
       <h2 className="text-center">Share This Sketch</h2>
       <InputGroup>
-      <Input value={shareUrl} disabled />
-      <Button color="primary" onClick={this.initiateCopy}>
-        <FontAwesomeIcon icon={faCopy} />
-        {' '}
-        Copy to Clipboard
-      </Button>
+        <Input value={shareUrl} disabled />
+        <Button color="primary" onClick={initiateCopy}>
+          <FontAwesomeIcon icon={faCopy} />
+          {' '}
+          Copy to Clipboard
+        </Button>
       </InputGroup>
       <hr />
-      <p className="text-center">{str.copyStatus}</p>
+      <p className="text-center">{copyStatus}</p>
     </ReactModal>
   );
 }
