957ddcca4e95cd7cdc268b878d37d8c305631a28
Zihan Xue <zihanxue.zx@gmail.com>
2022-11-08 17:10:56 -0800

    Minor style fix

--- src/reducers/userDataReducer.js
+++ src/reducers/userDataReducer.js
@@ -19,25 +19,23 @@ const initialState = {
 
 function userDataReducer(state = initialState, action) {
   switch (action.type) {
-  case LOAD_USER_DATA:
-    // pull all values we want to pay attention to out of the object
-    return { ...state, ...action.userData };
-  case CLEAR_USER_DATA:
-    return initialState;
-  case LOAD_FAILURE:
-    return { ...state, error: action.message };
-  case SET_DISPLAY_NAME: {
-    return { ...state, displayName: action.value };
-  }
-  case SET_PHOTO_NAME: {
-    return { ...state, photoName: action.photoName };
-  }
-  case SET_MOST_RECENT_PROGRAM:
-    return { ...state, mostRecentProgram: action.value };
-  case SET_CURRENT_CLASS:
-    return { ...state, currentClass: action.value };
-  default:
-    return state;
+    case LOAD_USER_DATA:
+      // pull all values we want to pay attention to out of the object
+      return { ...state, ...action.userData };
+    case CLEAR_USER_DATA:
+      return initialState;
+    case LOAD_FAILURE:
+      return { ...state, error: action.message };
+    case SET_DISPLAY_NAME:
+      return { ...state, displayName: action.value };
+    case SET_PHOTO_NAME:
+      return { ...state, photoName: action.photoName };
+    case SET_MOST_RECENT_PROGRAM:
+      return { ...state, mostRecentProgram: action.value };
+    case SET_CURRENT_CLASS:
+      return { ...state, currentClass: action.value };
+    default:
+      return state;
   }
 }
 export default userDataReducer;
