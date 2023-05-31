5c312e3c0f5f986aacecc2ed285adfa495c1801c
Tom Binford <tjbof123@gmail.com>
2023-05-31 12:36:30 -0700

    Update sketch deletion to avoid setMostRecentProgram With the changes for #746, setMostRecentProgram will update the URL but this is undesirable from the sketches page. Remove the mostRecentProgram accounting from the sketch deletion modal and do that work in the /editor path of the router.

--- src/components/Sketches/containers/ConfirmDeleteModalContainer.js
+++ src/components/Sketches/containers/ConfirmDeleteModalContainer.js
@@ -1,31 +1,11 @@
 import { connect } from 'react-redux';
 import { deleteProgram } from '../../../actions/programsActions';
-import { setMostRecentProgram } from '../../../actions/userDataActions';
-import * as fetch from '../../../lib/fetch';
 import ConfirmDeleteModal from '../components/ConfirmDeleteModal';
 
-const mapStateToProps = (state) => {
-  const { mostRecentProgram, uid } = state.userData;
-  const programKeys = state.programs.keySeq(); // this is an immutable sequence
-  return {
-    mostRecentProgram,
-    programKeys,
-    uid,
-  };
-};
+const mapStateToProps = (state) => ({ uid: state.userData.uid });
 
 const mapDispatchToProps = (dispatch) => ({
   deleteProgram: (program, data) => dispatch(deleteProgram(program, data)),
-  setMostRecentProgram: (value, uid) => {
-    try {
-      fetch.updateUserData(uid, { mostRecentProgram: value }).catch((err) => {
-        console.error(err);
-      });
-    } catch (err) {
-      console.error(err);
-    }
-    dispatch(setMostRecentProgram(value));
-  },
 });
 
 const ConfirmDeleteModalContainer = connect(
