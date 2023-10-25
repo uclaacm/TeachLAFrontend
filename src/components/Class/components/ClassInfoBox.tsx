300311299367ab1efa8efa382129a19f057aecc9
Michael Lan <michaellan202@gmail.com>
2023-10-24 18:49:43 -0700

    Convert ClassInfoBox.js to TypeScript

--- src/components/Class/components/ClassInfoBox.tsx
+++ src/components/Class/components/ClassInfoBox.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../../../styles/ClassPage.scss';
 
+interface ClassInfoBoxProps {
+  title: string;
+  children: React.ReactNode;
+};
+
 const ClassInfoBox = function ({
   title,
   children,
-}) {
+} : ClassInfoBoxProps) {
   return (
     <div className="class-info-box">
       <b className="section-header">{title}</b>
