1f3e45a2dd81cc3e27da5fafa84c9af281955e27
Michael Lan <michaellan202@gmail.com>
2023-10-25 15:04:21 -0700

    fix: Address linting issues with LoadingPage

--- src/components/common/LoadingPage.tsx
+++ src/components/common/LoadingPage.tsx
@@ -3,22 +3,12 @@ import { RingLoader } from 'react-spinners';
 import '../../styles/Loading.scss';
 import { GH_REPO_NAME } from '../../constants';
 
-/*
-	Props:
-		bgColor: string representing the color of the background of the img (can be hex color, rgb(r, g, b, a),
-    or color name) textColor: string representing the color of the text in the button
-    (can be hex color, rgb(r, g, b, a), or color name) imgSrc: string representing the location of the
-    img used as the icon (can be in the form of URL, path location, or data representing image)
-		textPadding: string representing padding to the left of the text, i.e. distance from the img
-    (give px units)
-*/
-
 interface LoadingProps {
-  showHelpText?: boolean;
-};
+  shouldShowHelpText: boolean;
+}
 
-const Loading = function (props : LoadingProps) {
-  const [showHelpText, setShowHelpText] = useState(props.showHelpText);
+const Loading = ({ shouldShowHelpText = false }: LoadingProps) => {
+  const [showHelpText, setShowHelpText] = useState(shouldShowHelpText);
 
   const timer = setTimeout(
     () => {
@@ -35,7 +25,7 @@ const Loading = function (props : LoadingProps) {
     <div className="Loading">
       <div className="Loading-title">Loading</div>
       <RingLoader color="#171124" size={250} loading />
-      { showHelpText && (
+      {showHelpText && (
         <p className="Loading-page-text" style={{ color: 'white' }}>
           Looks like loading is taking a bit long! If it takes too long, submit an issue on
           {' '}
