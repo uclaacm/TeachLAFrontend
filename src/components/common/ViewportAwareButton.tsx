1f4c9ec5fdaa2ddaebfc5ccdd257e34229721a53
LeroyBG <116683456+LeroyBG@users.noreply.github.com>
2023-10-31 10:21:20 -0700

    convert to typescript

---
+++ src/components/common/ViewportAwareButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+/** -------Props--------
+ * isSmall: dictates display of text field
+ * icon: desired icon (i.e. <FontAwesomeIcon icon={...} />)
+ * text: the text label for the button
+ */
+/** --------Optional props--------
+ * any props you would apply to a reactstrap Button.
+ */
+
+interface ViewportAwareButtonProps{
+  icon : React.JSX.Element,
+  text : string,
+  isSmall : boolean,
+  remainder : any,
+};
+
+const ViewportAwareButton = function (props : ViewportAwareButtonProps) {
+  // extract icon and text from props.
+  const {
+    icon, text, isSmall, ...remainder
+  } = props;
+
+  // render icon always, text only if not small.
+  return (
+    <Button {...remainder}>
+      {icon}
+
+      {!isSmall && (
+        <span className="viewport-aware-button-text">
+          {text}
+        </span>
+      )}
+    </Button>
+  );
+};
+
+export default ViewportAwareButton;
