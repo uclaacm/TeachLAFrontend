a7700018b08f17f4e5735daac09284817ee77710
Michael Lan <michaellan202@gmail.com>
2024-03-01 12:58:45 -0800

    fix: Use Vite BASE_URL env variable

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -19,16 +19,16 @@ const isLocalhost = Boolean(
 export default function register() {
   if (import.meta.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(import.meta.env.PUBLIC_URL, window.location);
-    if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
+    const baseUrl = new URL(import.meta.env.BASE_URL, window.location);
+    if (baseUrl.origin !== window.location.origin) {
+      // Our service worker won't work if BASE_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
       return;
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${import.meta.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${import.meta.env.BASE_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Lets check if a service worker still exists or not.
